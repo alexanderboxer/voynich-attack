5550ac7b391c84e21ef3d2756138a874cee72846
ALEXANDER BOXER <alexanderboxer@ALEXANDERs-MBP.lan>
2022-07-02 21:24:25 -0500

    typofix: f013r_idx -> f103r_idx

--- explorations/voynpy/corpora.py
+++ explorations/voynpy/corpora.py
@@ -14,12 +14,12 @@ vmspath = '../../transcription/vms.csv'
 vms = reftext.from_csv(vmspath, language = 'voynich', read_from_col = 3, comma_split_tokens = True)
 
 # vms1: Voynich up to f103r
-f013r_idx = vms.df[vms.df.folio == '103r'].index.tolist()[0]
-vms1_df = vms.df.iloc[:f013r_idx].copy()
+f103r_idx = vms.df[vms.df.folio == '103r'].index.tolist()[0]
+vms1_df = vms.df.iloc[:f103r_idx].copy()
 vms1 = reftext.from_dataframe(vms1_df, language = 'voynich', read_from_col = 3, comma_split_tokens = True) 
 
 # vms2: Voynich from f103r
-vms2_df = vms.df.iloc[f013r_idx:].copy()
+vms2_df = vms.df.iloc[f103r_idx:].copy()
 vms2 = reftext.from_dataframe(vms2_df, language = 'voynich', read_from_col = 3, comma_split_tokens = True) 
 
 # enoch: MS 3188 Enochian 
