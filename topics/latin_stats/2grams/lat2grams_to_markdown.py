da0bcd5b6d3c520b2409f684615c058fc0084b99
ALEXANDER BOXER <alexanderboxer@ALEXANDERs-MBP.lan>
2022-07-22 22:11:03 -0500

    lat 2grams

--- topics/latin_stats/2grams/lat2grams_to_markdown.py
+++ topics/latin_stats/2grams/lat2grams_to_markdown.py
@@ -1,5 +1,5 @@
 '''
-Latin 1-grams
+Latin 2-grams
 '''
 # ==============================================================================
 # Imports
@@ -13,20 +13,23 @@ from corpora import caesar, vitruvius
 # ==============================================================================
 # Combine dataframes
 # ==============================================================================
-df1 = caesar.chardf()
-df2 = vitruvius.chardf()
+df1 = caesar.chardf(2)
+df2 = vitruvius.chardf(2)
 dataframe_list = [df1, df2]
 dataframe_namelist = ['Caesar', 'Vitruvius']
 
 tot_df = pd.concat(dataframe_list, axis = 0).groupby('gram').agg({'n': 'sum'}).sort_values('n', ascending = False).reset_index()
 N = tot_df.n.sum()
+tot_df['gram'] = tot_df['gram'].apply(lambda x: x.replace('-',''))
 tot_df['%'] = tot_df['n'].apply(lambda x: '{:.2f}'.format(100 * x / N))
 tot_df['n'] = tot_df['n'].apply(lambda x: '{:,}'.format(x))
 tot_df['✧'] = ''
 
 df = tot_df.rename(columns = {'gram': 'all texts'})
 for qdf, name in zip(dataframe_list, dataframe_namelist):
-    qdf = qdf.reindex(range(df.shape[0]), fill_value = '').rename(columns = {'gram': name})
+    qdf = qdf.reindex(range(df.shape[0]), fill_value = '')
+    qdf['gram'] = qdf['gram'].apply(lambda x: x.replace('-',''))
+    qdf.rename(columns = {'gram': name}, inplace = True)
     qdf['n'] = qdf['n'].apply(lambda x: '{:,}'.format(int(x)) if x != '' else '')
     qdf['%'] = qdf.pct.astype(str).apply(lambda x: x if len(x) > 0 else '')
     qdf.drop('pct', axis = 1, inplace = True)
@@ -36,7 +39,6 @@ for qdf, name in zip(dataframe_list, dataframe_namelist):
 df = df.iloc[:,:-1]
 df['rank'] = df.index + 1
 df = df.set_index('rank').reset_index()
-
 
 # ==============================================================================
 # Convert to markdown
@@ -55,8 +57,8 @@ markdown_table = dataframe_to_markdown(df)
 # Description
 # ==============================================================================
 desc = ''
-desc += '[⇦ Back](https://github.com/alexanderboxer/voynich-attack/tree/main/topics/biblio) | [Table of Contents](https://github.com/alexanderboxer/voynich-attack) | [Next ⇨](https://github.com/alexanderboxer/voynich-attack/tree/main/topics/latin_stats/2grams)\n\n'
-desc += '## Latin Letter Frequencies\n\n'
+desc += '[⇦ Back](https://github.com/alexanderboxer/voynich-attack/tree/main/topics/latin_stats/1grams) | [Table of Contents](https://github.com/alexanderboxer/voynich-attack) | [Next ⇨](https://github.com/alexanderboxer/voynich-attack)\n\n'
+desc += '## Latin Bigram Frequencies\n\n'
 
 markdown_text = desc + markdown_table
 
