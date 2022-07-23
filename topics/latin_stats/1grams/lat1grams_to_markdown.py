9be124490ed49f0c6e35e9cc315b747240b06ba7
ALEXANDER BOXER <alexanderboxer@ALEXANDERs-MBP.lan>
2022-07-22 21:56:36 -0500

    latin 1grams

--- topics/latin_stats/1grams/lat1grams_to_markdown.py
+++ topics/latin_stats/1grams/lat1grams_to_markdown.py
@@ -1,5 +1,5 @@
 '''
-Voynich 1-grams
+Latin 1-grams
 '''
 # ==============================================================================
 # Imports
@@ -8,16 +8,35 @@ import pandas as pd
 
 import sys
 sys.path.insert(0, '../../../voynpy')
-from corpora import vms
+from corpora import caesar, vitruvius
 
 # ==============================================================================
-# Dataframe
+# Combine dataframes
 # ==============================================================================
-df = vms.chardf().astype(str)
-df = df[df.gram.apply(lambda x: '?' not in x)].reset_index(drop = True)
-df['rank'] = 1 + df.index
-df = df.set_index('rank').reset_index().rename(columns = {'pct':'%'})
-df['n'] = df['n'].apply(lambda x: '{:,}'.format(int(x)))
+df1 = caesar.chardf()
+df2 = vitruvius.chardf()
+dataframe_list = [df1, df2]
+dataframe_namelist = ['Caesar', 'Vitruvius']
+
+tot_df = pd.concat(dataframe_list, axis = 0).groupby('gram').agg({'n': 'sum'}).sort_values('n', ascending = False).reset_index()
+N = tot_df.n.sum()
+tot_df['%'] = tot_df['n'].apply(lambda x: '{:.2f}'.format(100 * x / N))
+tot_df['n'] = tot_df['n'].apply(lambda x: '{:,}'.format(x))
+tot_df['✧'] = ''
+
+df = tot_df.rename(columns = {'gram': 'all texts'})
+for qdf, name in zip(dataframe_list, dataframe_namelist):
+    qdf = qdf.reindex(range(df.shape[0]), fill_value = '').rename(columns = {'gram': name})
+    qdf['n'] = qdf['n'].apply(lambda x: '{:,}'.format(int(x)) if x != '' else '')
+    qdf['%'] = qdf.pct.astype(str).apply(lambda x: x if len(x) > 0 else '')
+    qdf.drop('pct', axis = 1, inplace = True)
+    qdf['✧'] = ''
+    df = pd.concat([df, qdf], axis = 1)
+
+df = df.iloc[:,:-1]
+df['rank'] = df.index + 1
+df = df.set_index('rank').reset_index()
+
 
 # ==============================================================================
 # Convert to markdown
@@ -25,7 +44,7 @@ df['n'] = df['n'].apply(lambda x: '{:,}'.format(int(x)))
 def dataframe_to_markdown(dataframe):
     s = dataframe.to_csv(sep = '|', index = False).replace('\n','|\n|')
     table_header = '|' + s.split('\n')[0]
-    table_formatting = '|' + ':-:|' * df.shape[1]
+    table_formatting = '|' + ':-:|' * dataframe.shape[1]
     table_body = s.split('\n', maxsplit = 1)[1].rsplit('\n', maxsplit = 1)[0]
     markdown_table = table_header + '\n' + table_formatting + '\n' + table_body
     return markdown_table
@@ -38,9 +57,6 @@ markdown_table = dataframe_to_markdown(df)
 desc = ''
 desc += '[⇦ Back](https://github.com/alexanderboxer/voynich-attack/tree/main/topics/biblio) | [Table of Contents](https://github.com/alexanderboxer/voynich-attack) | [Next ⇨](https://github.com/alexanderboxer/voynich-attack)\n\n'
 desc += '## Latin Letter Frequencies\n\n'
-desc += 'Our Voynich [transcription](https://github.com/alexanderboxer/voynich-attack/tree/main/transcription) consists of 147,485 comma-separated characters.'
-desc += ' Of these, about 0.5% (756) are marked with a `?` to indicate an unclear or ambiguous reading.'
-desc += ' The remaining character-set consists of roughly 21 “standard” and another 15 or so non-standard characters.\n\n'
 
 markdown_text = desc + markdown_table
 
