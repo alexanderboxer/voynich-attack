4d6d2f436a1282fb5d58c943b827901ae0d41e0c
chris <cgoldman@neurocoreai.com>
2026-04-06 12:25:49 +0000

    Add BMP PUA Unicode mapping support in corpora.py and new unicode_dict_bmp.json
    By shifting to the Basic Multilingual Plane the PUA code points used for the Voynich glyphs now take 3 bytes instead of 4 bytes. This mapping should be used with the CustomVoynichUnicode.ttf (from the voynich-entropy repo) when rendering the glyphs

--- voynpy/corpora.py
+++ voynpy/corpora.py
@@ -68,11 +68,17 @@ w7 = reftext.from_csv(w7path, language = 'voynich', read_from_col = 4, comma_spl
 w7.df.columns = ['folio','side','par','line'] + ['t{}'.format(k + 1) for k in range(w7.df.shape[1] - 4)]
 w7.df = w7.df.fillna('$')
 
-# vms_unicode: Voynich with PUA Unicode mapping
+# vms_unicode: Voynich with PUA Unicode mapping (Supplementary PUA-B, U+FF400–FF51F)
 unicode_dict_path = _root / 'transcription/unicode_dict.json'
 with open(unicode_dict_path, 'r') as f:
     _unicode_char_map = json.load(f)
 vms_unicode = reftext.from_mapped(vms, _unicode_char_map, language='voynich_unicode')
+
+# vms_unicode_bmp: Voynich with BMP PUA mapping (U+E000–E11F)
+unicode_dict_bmp_path = _root / 'transcription/unicode_dict_bmp.json'
+with open(unicode_dict_bmp_path, 'r') as f:
+    _unicode_char_map_bmp = json.load(f)
+vms_unicode_bmp = reftext.from_mapped(vms, _unicode_char_map_bmp, language='voynich_unicode_bmp')
 
 
 #----------
